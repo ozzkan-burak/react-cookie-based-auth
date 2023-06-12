41bd11932295009a46cdd55bfd26b032b8d05f65
ozzkan-burak <brk.ozzkan@gmail.com>
2023-06-12 20:55:58 +0300

    episode4

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "597ae9e650069409"; }
+/******/ 		__webpack_require__.h = function() { return "3110cc2b38e3b1e6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,18 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'antd/dist/reset.css';
 import "../public/css/styles.css";
+import TopNav from '../components/TopNav';
+
 
 
 const MyApp = ({Component, pageProps}) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <TopNav />
+     <Component {...pageProps} />
+    </>
+    
+  )
 }
 
 export default MyApp;
