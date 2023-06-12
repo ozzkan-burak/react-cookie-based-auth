162bf0b5280ab8a63b7e1071c1bb504c80be7631
ozzkan-burak <brk.ozzkan@gmail.com>
2023-06-12 22:01:18 +0300

    episode5

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "30bc628d175d18ff"; }
+/******/ 		__webpack_require__.h = function() { return "3b0ba66a0b69e380"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/register.jsx
+++ pages/register.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, {useRef} from 'react';
 
 const RegisterPage = () => {
+
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const passRef = useRef();
+
   return (
-    <h1 className="jumbotron rext-center bg-primary square">RegisterPage</h1>
+    <>
+      <h1 className="jumbotron rext-center bg-primary square">RegisterPage</h1>
+      <div className="container col-md-4 offset-md-4 pb-5">
+        <form onSubmit={handleSubmit}>
+          <input type="text" ref={nameRef} className="form-control mb-4 p4" value={'test'} />
+          <input type="email" ref={emailRef} className="form-control mb-4 p4" value={'test'} />
+          <input type="password" ref={passRef} className="form-control mb-4 p4" value={'test'} />
+        </form>
+      </div>
+    </>
   )
 }
 
